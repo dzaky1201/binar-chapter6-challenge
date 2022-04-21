a027adbf97a3fe6837a867ef8b4cf5ce5f8a3d28
dzaky1201 <dzakyhdr@gmail.com>
2022-04-21 08:27:57 +0700

    login session

--- app/src/main/java/com/dzakyhdr/moviedb/ui/login/LoginFragment.kt
+++ app/src/main/java/com/dzakyhdr/moviedb/ui/login/LoginFragment.kt
@@ -5,18 +5,88 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.dzakyhdr.moviedb.R
+import com.dzakyhdr.moviedb.databinding.FragmentLoginBinding
+import com.dzakyhdr.moviedb.resource.Status
+import com.dzakyhdr.moviedb.ui.ViewModelFactory
+import com.dzakyhdr.moviedb.ui.register.RegisterViewModel
+import com.dzakyhdr.moviedb.utils.SharedPreference
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.Executors
 
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: LoginViewModel
+    private var sharedPref: SharedPreference? = null
+    private var status = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val factory = ViewModelFactory(view.context)
+        viewModel = ViewModelProvider(requireActivity(), factory)[LoginViewModel::class.java]
+        sharedPref = SharedPreference(view.context)
+
+        status = sharedPref?.getPrefKeyStatus("login_status") == true
+
+        if (status) {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        binding.btnLogin.setOnClickListener {
+
+                viewModel.login(
+                    binding.edtEmail.text.toString(),
+                    binding.edtPassword.text.toString()
+                )
+
+
+            viewModel.loginStatus.observe(viewLifecycleOwner) {
+                when (it.status) {
+                    Status.SUCCESS -> {
+                        if (it.data != null) {
+                            sharedPref?.saveKey(it.data)
+                            sharedPref?.saveKeyState(true)
+                            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                        } else {
+                            Snackbar.make(
+                                binding.root,
+                                "User Tidak Ditemukan",
+                                Snackbar.LENGTH_LONG
+                            )
+                                .show()
+                        }
+                    }
+                    Status.ERROR -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {}
+                }
+            }
+        }
+
+        binding.txtRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
---
+++ app/src/main/java/com/dzakyhdr/moviedb/utils/SharedPreference.kt
@@ -0,0 +1,47 @@
+package com.dzakyhdr.moviedb.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import com.dzakyhdr.moviedb.data.local.auth.User
+
+class SharedPreference(context: Context) {
+
+    private val pref = "authentication"
+    private val sharedPreference = context.getSharedPreferences(pref, Context.MODE_PRIVATE)
+
+    @SuppressLint("CommitPrefEdits")
+    fun saveKey(user: User) {
+        val editor = sharedPreference.edit()
+        editor.putString("email", user.email)
+        editor.putString("password", user.password)
+        editor.putString("address", user.address)
+        editor.putString("fullname", user.fullname)
+        editor.putString("date", user.ttl)
+        editor.putString("username", user.username)
+        editor.apply()
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun saveKeyState(status: Boolean){
+        val editor = sharedPreference.edit()
+        editor.putBoolean("login_status", status)
+        editor.apply()
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun getPrefKey(key: String): String? {
+        return sharedPreference.getString(key, "data kosong")
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun getPrefKeyStatus(key: String): Boolean{
+        return sharedPreference.getBoolean(key, false)
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun clearUsername(){
+        val editor = sharedPreference.edit()
+        editor.clear()
+        editor.apply()
+    }
+}
